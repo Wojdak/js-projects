77358a2a9471033e2941a95df3c0708740b871c4
Wojdak <jakub.wojdak11@gmail.com>
2023-10-06 11:26:12 +0200

    lab_1_second_version

--- lab-1/main.js
+++ lab-1/main.js
@@ -1,16 +1,23 @@
-const btn = document.querySelector('#btn')
 const wynikiPojemnik = document.querySelector('#wynik')
 
-btn.addEventListener('click', () => {
-    let num1 = parseInt(document.querySelector('#liczba1').value)
-    let num2 = parseInt(document.querySelector('#liczba2').value)
-    let num3 = parseInt(document.querySelector('#liczba3').value)
-    let num4 = parseInt(document.querySelector('#liczba4').value)
+const inputs = document.querySelectorAll('input');
 
+inputs.forEach(input => {
+    input.addEventListener('input', updateResults);
+});
+
+function updateResults() {
+    const num1 = Number(document.querySelector('#liczba1').value)
+    const num2 = Number(document.querySelector('#liczba2').value)
+    const num3 = Number(document.querySelector('#liczba3').value)
+    const num4 = Number(document.querySelector('#liczba4').value)
+    
     const suma = num1+num2+num3+num4
 
-    document.querySelector('#suma').innerHTML+=suma
-    document.querySelector('#srednia').innerHTML+=suma/4
-    document.querySelector('#min').innerHTML+=Math.min(num1,num2,num3,num4)
-    document.querySelector('#max').innerHTML+=Math.max(num1,num2,num3,num4)
-})
+    document.querySelector('#suma').innerHTML= `Suma: ${suma}`
+    document.querySelector('#srednia').innerHTML=`Srednia: ${suma/4}`
+    document.querySelector('#min').innerHTML=`Mina: ${Math.min(num1,num2,num3,num4)}`
+    document.querySelector('#max').innerHTML=`Max: ${Math.max(num1,num2,num3,num4)}` 
+}
+
+    
