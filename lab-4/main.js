be6d24c34da834fcf489369f396b788dd1031b38
Wojdak <jakub.wojdak11@gmail.com>
2023-12-11 17:24:32 +0100

    lab_4_second_version

--- lab-4/main.js
+++ lab-4/main.js
@@ -8,8 +8,9 @@ function addNote() {
     const color = document.getElementById("color").value
     const pin = document.getElementById("pin").checked
     const date = new Date().toLocaleString()
+    const tags = document.getElementById("tags").value.trim().split(",") // Rozdziel tagi po przecinku
 
-    const note = { title, content, color, pin, date }
+    const note = { title, content, color, pin, date, tags}
 
     let notes = JSON.parse(localStorage.getItem("notes")) || []
 
@@ -45,10 +46,13 @@ function createNoteElement(note, index) {
     noteElement.classList.add("note")
     noteElement.style.backgroundColor = note.color
 
+    const tagsContent = note.tags.length > 0 ? note.tags.join(', ') : ''
+
     noteElement.innerHTML = `
         <div class="note-content">
             <div class="note-title">${note.title}</div>
             <div>${note.content}</div>
+            <div class="note-tags">${tagsContent}</div>
         </div>
         <div class="note-actions">
             <div>${note.date}</div>
@@ -65,4 +69,29 @@ function deleteNote(index) {
     localStorage.setItem("notes", JSON.stringify(notes))
 
     displayNotes()
+}
+
+function searchNotes() {
+    const searchText = document.getElementById("search").value.toLowerCase()
+    let notes = JSON.parse(localStorage.getItem("notes")) || []
+
+    const filteredNotes = notes.filter(note => {
+        const content = note.content.toLowerCase()
+        const title = note.title.toLowerCase()
+        const tags = note.tags ? note.tags.join(', ') : ''; // Połącz tagi za pomocą przecinków
+
+        return content.includes(searchText) || title.includes(searchText) || tags.includes(searchText)
+    })
+
+    displayFilteredNotes(filteredNotes)
+}
+
+function displayFilteredNotes(filteredNotes) {
+    const noteList = document.getElementById("note-list")
+    noteList.innerHTML = ""
+
+    filteredNotes.forEach((note, index) => {
+        const noteElement = createNoteElement(note, index)
+        noteList.appendChild(noteElement)
+    })
 }
